443aacc7a2f8ec48acceceecdcabee5c9448d543
Saifullah Ahmed <saifullahahmed380@gmail.com>
2025-11-29 16:41:39 +0500

    fixed import bugs

--- components/service/case-studies/CaseStudy.tsx
+++ components/service/case-studies/CaseStudy.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { caseStudies } from "@/components/service/case studies/caseStudies";
+import { caseStudies } from "@/components/service/case-studies/caseStudies";
 
 export default function CaseStudies() {
     return (
