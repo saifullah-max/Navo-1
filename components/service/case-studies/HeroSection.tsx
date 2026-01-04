f9474765f863de4cd24b3d8a3faef1dafabbd717
abdullahahmed <35358789+AbdullahAhmad1@users.noreply.github.com>
2026-01-04 16:11:16 +0500

    added hero images on case study

--- components/service/case-studies/HeroSection.tsx
+++ components/service/case-studies/HeroSection.tsx
@@ -2,10 +2,30 @@ import React from 'react';
 
 const HeroSection = () => {
     return (
-        <div className="relative pb-24 md:pb-0 bg-gradient-to-r md:mt-24 mt-20 bg-[#041938] md:min-h-[800px] flex flex-col items-center overflow-hidden">
-{/* <div className='bg-[#]' /> */}
+        <div
+            className="
+                relative
+                pb-24
+                md:pb-0
+                bg-[#041938]
+                mt-20
+                md:mt-24
+                min-h-[700px]
+                sm:min-h-[750px]
+                md:min-h-[800px]
+                flex flex-col
+                items-center
+                overflow-hidden
+                bg-[url('/case-study-sm.png')]
+                lg:bg-[url('/case-study-md.png')]
+                2xl:bg-[url('/case-study-lg.png')]
+                bg-center
+                bg-cover
+            "
+        >
+            {/* <div className='bg-[#]' /> */}
         </div>
     );
-}
+};
 
 export default HeroSection;
--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-  // Optimize for production
-  swcMinify: true,
-  // Output config for Vercel
   output: 'standalone',
-}
+};
 
-export default nextConfig
+export default nextConfig;
